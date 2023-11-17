6c2b0d9c27da59087d583fc1ab37f23a4aba25bc
Guilherme Saturno <guilhermebsaturno@gmail.com>
2023-11-17 16:41:16 +0000

    Limit frame rate

--- main.cpp
+++ main.cpp
@@ -4,16 +4,30 @@ Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
-	game = new Game();
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	
+	Uint32 frameStart;
+	int frameTime;
 
+	game = new Game();
 	game->init("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (game->running())
 	{
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->start();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) 
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->quit();
